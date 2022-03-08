12118afe95a60acdd52f6b09dec020f846d14e87
Norbert Csibra <ncsibra@users.noreply.github.com>
2022-03-08 11:51:00 +0100

    Sync with gitlab (#1)

--- echobind/echo_bind.go
+++ echobind/echo_bind.go
@@ -1,6 +1,9 @@
 package echobind
 
 import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
 	"strings"
 
 	"github.com/labstack/echo/v4"
@@ -8,12 +11,52 @@ import (
 	"github.com/proemergotech/bind/internal"
 )
 
-func JSONContentTypeMiddleware() echo.MiddlewareFunc {
+type settings struct {
+	readBody bool
+}
+
+type Option func(*settings)
+
+func ReadBody() Option {
+	return func(opts *settings) {
+		opts.readBody = true
+	}
+}
+
+func JSONContentTypeMiddleware(options ...Option) echo.MiddlewareFunc {
+	s := &settings{}
+
+	for _, opt := range options {
+		opt(s)
+	}
+
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(eCtx echo.Context) error {
 			req := eCtx.Request()
-
 			contentType := req.Header.Get(echo.HeaderContentType)
+
+			if req.Body == nil || req.Body == http.NoBody || req.ContentLength == 0 || req.Method == http.MethodGet {
+				if contentType != "" {
+					return internal.ContentTypeWithoutBodyError{}.E()
+				}
+				return next(eCtx)
+			}
+
+			if s.readBody && req.ContentLength == -1 {
+				body, err := ioutil.ReadAll(req.Body)
+				if err != nil {
+					return internal.CannotReadBodyError{}.E()
+				}
+				req.Body = ioutil.NopCloser(bytes.NewBuffer(body))
+
+				if string(body) == "" {
+					if contentType != "" {
+						return internal.ContentTypeWithoutBodyError{}.E()
+					}
+					return next(eCtx)
+				}
+			}
+
 			if !strings.HasPrefix(contentType, echo.MIMEApplicationJSON) {
 				return internal.JSONContentTypeError{ContentType: contentType}.E()
 			}
--- internal/error.go
+++ internal/error.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 
 	"github.com/labstack/echo/v4"
-	"github.com/proemergotech/errors"
+	"github.com/proemergotech/errors/v2"
 )
 
 const (
@@ -12,7 +12,31 @@ const (
 	ErrHTTPCode = "http_code"
 
 	ErrOnlyJSONContentTypeAllowed = "ERR_ONLY_JSON_CONTENT_TYPE_ALLOWED"
+	ErrContentTypeWithoutBody     = "ERR_CONTENT_TYPE_WITHOUT_BODY"
+	ErrCannotReadBody             = "ERR_CANNOT_READ_BODY"
 )
+
+type ContentTypeWithoutBodyError struct{}
+
+func (e ContentTypeWithoutBodyError) E() error {
+	msg := "empty body: content type header only allowed with body"
+
+	return errors.WithFields(
+		errors.New(msg),
+		ErrHTTPCode, 400,
+		ErrCode, ErrContentTypeWithoutBody,
+	)
+}
+
+type CannotReadBodyError struct{}
+
+func (e CannotReadBodyError) E() error {
+	return errors.WithFields(
+		errors.New("cannot read body"),
+		ErrHTTPCode, 500,
+		ErrCode, ErrCannotReadBody,
+	)
+}
 
 type JSONContentTypeError struct {
 	ContentType string
